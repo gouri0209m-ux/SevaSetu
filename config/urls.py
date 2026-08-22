b49ac10f23ea9cbf928b24a71eeacf32a05be5be
Gouri Mishra <gouri0209m@gmail.com>
2026-08-23 01:08:49 +0530

    Added authentication and dashboard

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', include('core.urls')),
     path( 'campaigns/', include('campaigns.urls')),
     path( 'accounts/', include('accounts.urls')),
+    path('', include('engagement.urls')),
 ]
