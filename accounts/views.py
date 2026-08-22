9190e26c822e5744a4b243ee30bc1f1e3c8833d6
Gouri Mishra <gouri0209m@gmail.com>
2026-08-23 00:47:28 +0530

    Added authentication and dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+
+from .forms import RegisterForm
+
+
+def register(request):
+
+    if request.method == 'POST':
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            user.set_password(
+                form.cleaned_data['password']
+            )
+
+            user.save()
+
+            login(request, user)
+
+            return redirect('home')
+
+    else:
+
+        form = RegisterForm()
+
+    return render(
+        request,
+        'accounts/register.html',
+        {'form': form}
+    )
+
+
+def login_view(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user:
+
+            login(request, user)
+
+            return redirect('home')
+
+    return render(
+        request,
+        'accounts/login.html'
+    )
+
+
+def logout_view(request):
+
+    logout(request)
+
+    return redirect('home')
--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from campaigns.models import Campaign
+from engagement.models import VolunteerApplication
+from engagement.models import Donation
 from engagement.models import ContactMessage
+
+
 
 def home(request):
     return render(request, 'core/home.html')
@@ -32,4 +37,29 @@ def contact(request):
     return render(
         request,
         'core/contact.html'
+    )
+    
+    
+def dashboard(request):
+
+    context = {
+
+        'campaign_count':
+        Campaign.objects.count(),
+
+        'volunteer_count':
+        VolunteerApplication.objects.count(),
+
+        'donation_count':
+        Donation.objects.count(),
+
+        'message_count':
+        ContactMessage.objects.count(),
+
+    }
+
+    return render(
+        request,
+        'core/dashboard.html',
+        context
     )
