05a1d8c81b71b0a34055cc408ff587ad49cc57c2
gouri0209m-ux <gouri0209m@gmail.com>
2026-08-23 22:13:36 +0530

    Clean project URL configuration

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path( 'campaigns/', include('campaigns.urls')),
-    path( 'accounts/', include('accounts.urls')),
+    path('campaigns/', include('campaigns.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('engagement.urls')),
 ]
